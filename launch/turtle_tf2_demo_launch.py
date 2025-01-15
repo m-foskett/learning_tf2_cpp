04d87f4034e10db47fc3d6357fbb9837cb71852c
m-foskett <markryanfoskett@gmail.com>
2025-01-15 14:51:12 +1100

    Added a listener node to listen to the broadcast transform

--- launch/turtle_tf2_demo_launch.py
+++ launch/turtle_tf2_demo_launch.py
@@ -1,4 +1,6 @@
 from launch import LaunchDescription # Generic launching framework "launch" (not ROS2 specific)
+from launch.actions import DeclareLaunchArgument # Launch arguments
+from launch.substitutions import LaunchConfiguration # Launch configuration
 from launch_ros.actions import Node # ROS2 specific launching actions
 
 
@@ -17,6 +19,29 @@ def generate_launch_description():
             name='broadcaster1',
             parameters=[
                 {'turtlename': 'turtle1'}
+            ]
+        ),
+        # Declare a launch argument to pass the target frame name
+        DeclareLaunchArgument(
+            'target_frame', default_value='turtle1',
+            description='Target frame name.'
+        ),
+        # Launching the turtle_tf2_listener node for a second turtle
+        Node(
+            package='learning_tf2_cpp',
+            executable='turtle_tf2_broadcaster',
+            name='broadcaster2',
+            parameters=[
+                {'turtlename': 'turtle2'}
+            ]
+        ),
+        # Launching the turtle_tf2_listener node to subscribe to the transforms
+        Node(
+            package='learning_tf2_cpp',
+            executable='turtle_tf2_listener',
+            name='listener',
+            parameters=[
+                {'target_frame': LaunchConfiguration('target_frame')}
             ]
         ),
     ])
